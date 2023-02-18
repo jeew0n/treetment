30b3086ea2f8f5e88f321a1aa62faa0076cd0c34
ralenkovara <ralenk@gmail.com>
2023-02-17 23:09:44 -0800

    Added Routers
    Added routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Test writing - Jeewon
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        //Home Page
+        <Route exact path="/">
+          <h1>Home page</h1>
+            <div className="App">
+              <header className="App-header">
+              <p>
+              Test writing - Jeewon
+              </p>
+              </header>
+            </div>
+        </Route>
+
+        //Patient Page
+        <Route path="/about">
+          <h1>About page</h1>
+        </Route>
+
+        
+        <Route path="/contact">
+          <h1>Contact page</h1>
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
